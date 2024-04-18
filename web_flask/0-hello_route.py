152e012e542e4da2f142675187aba6bcd51d2864
Romeo509 <romeoameku503@gmail.com>
2024-04-18 02:11:07 +0000

    0-hello_route.py updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" Start the fask application """
+""" Start the Flask application """
 
 from flask import Flask
 
-
+# Create a Flask application object named app
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
+# Define the route for /airbnb-onepage/
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
-    """return Hello HBNB"""
+    """ Return Hello HBNB """
     return "Hello HBNB!"
 
-
+# Ensure the Flask application is run when this script is executed directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
